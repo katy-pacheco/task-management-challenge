83e98d452da9b759540dc8e1fc74d27dcde786d5
Katy Pacheco <katypacheco@ravn.co>
2025-06-02 21:14:52 -0600

    feat: improve SearchBar component with focus handling

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,20 +1,22 @@
-import { faker } from "@faker-js/faker";
 import {
   RiCloseCircleLine,
   RiNotification3Line,
   RiSearchLine,
 } from "@remixicon/react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Avatar from "../avatar/avatar";
 import styles from "./search-bar.module.css";
 
 interface SearchInput {
   search: string;
 }
 
-const avatar = faker.image.avatar();
-
 export default function SearchBar() {
-  const { register, handleSubmit, reset } = useForm<SearchInput>();
+  const { register, handleSubmit, reset, watch } = useForm<SearchInput>();
+  const [isFocused, setIsFocused] = useState(false);
+
+  const searchValue = watch("search");
 
   function onSubmit(data: SearchInput) {
     console.log("Search query:", data.search);
@@ -28,14 +30,18 @@ export default function SearchBar() {
         placeholder="Search"
         {...register("search")}
         className={styles.formInput}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
       <div className={styles.formIconContainer}>
-        <RiCloseCircleLine
-          className={styles.formIcon}
-          onClick={() => reset()}
-        />
+        {(searchValue || isFocused) && (
+          <RiCloseCircleLine
+            className={styles.formIcon}
+            onClick={() => reset()}
+          />
+        )}
         <RiNotification3Line className={styles.formIcon} />
-        <img src={avatar} alt="profile" className={styles.formAvatar} />
+        <Avatar size={"medium"} />
       </div>
     </form>
   );
