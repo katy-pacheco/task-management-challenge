ac13fbee3e40ac183d178d7a90832205ee408b2a
Katy Pacheco <katypacheco@ravn.co>
2025-06-09 00:02:07 -0600

    feat: add GraphQL mutations and queries

---
+++ src/graphql/mutations/create-task.graphql.generated.ts
@@ -0,0 +1,198 @@
+import * as Types from '../../types/graphql';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
+};
+
+export type CreateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate: Scalars['DateTime']['input'];
+  name: Scalars['String']['input'];
+  pointEstimate: PointEstimate;
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+export type DeleteTaskInput = {
+  id: Scalars['String']['input'];
+};
+
+export type FilterTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  name?: InputMaybe<Scalars['String']['input']>;
+  ownerId?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTask: Task;
+  deleteTask: Task;
+  updateTask: Task;
+};
+
+
+export type MutationCreateTaskArgs = {
+  input: CreateTaskInput;
+};
+
+
+export type MutationDeleteTaskArgs = {
+  input: DeleteTaskInput;
+};
+
+
+export type MutationUpdateTaskArgs = {
+  input: UpdateTaskInput;
+};
+
+/** Estimate point for a task */
+export enum PointEstimate {
+  Eight = 'EIGHT',
+  Four = 'FOUR',
+  One = 'ONE',
+  Two = 'TWO',
+  Zero = 'ZERO'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  profile: User;
+  tasks: Array<Task>;
+  users: Array<User>;
+};
+
+
+export type QueryTasksArgs = {
+  input: FilterTaskInput;
+};
+
+/** Status for Task */
+export enum Status {
+  Backlog = 'BACKLOG',
+  Cancelled = 'CANCELLED',
+  Done = 'DONE',
+  InProgress = 'IN_PROGRESS',
+  Todo = 'TODO'
+}
+
+export type Task = {
+  __typename?: 'Task';
+  assignee?: Maybe<User>;
+  createdAt: Scalars['DateTime']['output'];
+  creator: User;
+  dueDate: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  pointEstimate: PointEstimate;
+  position: Scalars['Float']['output'];
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+/** Enum for tags for tasks */
+export enum TaskTag {
+  Android = 'ANDROID',
+  Ios = 'IOS',
+  NodeJs = 'NODE_JS',
+  Rails = 'RAILS',
+  React = 'REACT'
+}
+
+export type UpdateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['String']['input'];
+  name?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  position?: InputMaybe<Scalars['Float']['input']>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  avatar?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['DateTime']['output'];
+  email: Scalars['String']['output'];
+  fullName: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  type: UserType;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+/** Type of the User */
+export enum UserType {
+  Admin = 'ADMIN',
+  Candidate = 'CANDIDATE'
+}
+
+
+export const CreateTaskDocument = gql`
+    mutation CreateTask($input: CreateTaskInput!) {
+  createTask(input: $input) {
+    id
+    name
+    status
+    dueDate
+    pointEstimate
+    assignee {
+      id
+      fullName
+    }
+    tags
+    createdAt
+  }
+}
+    `;
+export type CreateTaskMutationFn = Apollo.MutationFunction<CreateTaskMutation, CreateTaskMutationVariables>;
+
+/**
+ * __useCreateTaskMutation__
+ *
+ * To run a mutation, you first call `useCreateTaskMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateTaskMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createTaskMutation, { data, loading, error }] = useCreateTaskMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateTaskMutation(baseOptions?: Apollo.MutationHookOptions<CreateTaskMutation, CreateTaskMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateTaskMutation, CreateTaskMutationVariables>(CreateTaskDocument, options);
+      }
+export type CreateTaskMutationHookResult = ReturnType<typeof useCreateTaskMutation>;
+export type CreateTaskMutationResult = Apollo.MutationResult<CreateTaskMutation>;
+export type CreateTaskMutationOptions = Apollo.BaseMutationOptions<CreateTaskMutation, CreateTaskMutationVariables>;
+export type CreateTaskMutationVariables = Types.Exact<{
+  input: Types.CreateTaskInput;
+}>;
+
+
+export type CreateTaskMutation = { __typename?: 'Mutation', createTask: { __typename?: 'Task', id: string, name: string, status: Types.Status, dueDate: any, pointEstimate: Types.PointEstimate, tags: Array<Types.TaskTag>, createdAt: any, assignee?: { __typename?: 'User', id: string, fullName: string } | null } };
---
+++ src/graphql/queries/get-users.graphql.generated.generated.ts
@@ -0,0 +1,198 @@
+import * as Types from '../../types/graphql';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
+};
+
+export type CreateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate: Scalars['DateTime']['input'];
+  name: Scalars['String']['input'];
+  pointEstimate: PointEstimate;
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+export type DeleteTaskInput = {
+  id: Scalars['String']['input'];
+};
+
+export type FilterTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  name?: InputMaybe<Scalars['String']['input']>;
+  ownerId?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTask: Task;
+  deleteTask: Task;
+  updateTask: Task;
+};
+
+
+export type MutationCreateTaskArgs = {
+  input: CreateTaskInput;
+};
+
+
+export type MutationDeleteTaskArgs = {
+  input: DeleteTaskInput;
+};
+
+
+export type MutationUpdateTaskArgs = {
+  input: UpdateTaskInput;
+};
+
+/** Estimate point for a task */
+export enum PointEstimate {
+  Eight = 'EIGHT',
+  Four = 'FOUR',
+  One = 'ONE',
+  Two = 'TWO',
+  Zero = 'ZERO'
+}
+
+export type Query = {
+  __typename?: 'Query';
+  profile: User;
+  tasks: Array<Task>;
+  users: Array<User>;
+};
+
+
+export type QueryTasksArgs = {
+  input: FilterTaskInput;
+};
+
+/** Status for Task */
+export enum Status {
+  Backlog = 'BACKLOG',
+  Cancelled = 'CANCELLED',
+  Done = 'DONE',
+  InProgress = 'IN_PROGRESS',
+  Todo = 'TODO'
+}
+
+export type Task = {
+  __typename?: 'Task';
+  assignee?: Maybe<User>;
+  createdAt: Scalars['DateTime']['output'];
+  creator: User;
+  dueDate: Scalars['DateTime']['output'];
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  pointEstimate: PointEstimate;
+  position: Scalars['Float']['output'];
+  status: Status;
+  tags: Array<TaskTag>;
+};
+
+/** Enum for tags for tasks */
+export enum TaskTag {
+  Android = 'ANDROID',
+  Ios = 'IOS',
+  NodeJs = 'NODE_JS',
+  Rails = 'RAILS',
+  React = 'REACT'
+}
+
+export type UpdateTaskInput = {
+  assigneeId?: InputMaybe<Scalars['String']['input']>;
+  dueDate?: InputMaybe<Scalars['DateTime']['input']>;
+  id: Scalars['String']['input'];
+  name?: InputMaybe<Scalars['String']['input']>;
+  pointEstimate?: InputMaybe<PointEstimate>;
+  position?: InputMaybe<Scalars['Float']['input']>;
+  status?: InputMaybe<Status>;
+  tags?: InputMaybe<Array<TaskTag>>;
+};
+
+export type User = {
+  __typename?: 'User';
+  avatar?: Maybe<Scalars['String']['output']>;
+  createdAt: Scalars['DateTime']['output'];
+  email: Scalars['String']['output'];
+  fullName: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  type: UserType;
+  updatedAt: Scalars['DateTime']['output'];
+};
+
+/** Type of the User */
+export enum UserType {
+  Admin = 'ADMIN',
+  Candidate = 'CANDIDATE'
+}
+
+
+export const GetUsersDocument = gql`
+    query getUsers {
+  users {
+    id
+    fullName
+    email
+    avatar
+    type
+    createdAt
+    updatedAt
+  }
+}
+    `;
+
+/**
+ * __useGetUsersQuery__
+ *
+ * To run a query within a React component, call `useGetUsersQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetUsersQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetUsersQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetUsersQuery(baseOptions?: Apollo.QueryHookOptions<GetUsersQuery, GetUsersQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUsersQuery, GetUsersQueryVariables>(GetUsersDocument, options);
+      }
+export function useGetUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUsersQuery, GetUsersQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUsersQuery, GetUsersQueryVariables>(GetUsersDocument, options);
+        }
+export function useGetUsersSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetUsersQuery, GetUsersQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetUsersQuery, GetUsersQueryVariables>(GetUsersDocument, options);
+        }
+export type GetUsersQueryHookResult = ReturnType<typeof useGetUsersQuery>;
+export type GetUsersLazyQueryHookResult = ReturnType<typeof useGetUsersLazyQuery>;
+export type GetUsersSuspenseQueryHookResult = ReturnType<typeof useGetUsersSuspenseQuery>;
+export type GetUsersQueryResult = Apollo.QueryResult<GetUsersQuery, GetUsersQueryVariables>;
+export type GetUsersQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type GetUsersQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', id: string, fullName: string, email: string, avatar?: string | null, type: Types.UserType, createdAt: any, updatedAt: any }> };
