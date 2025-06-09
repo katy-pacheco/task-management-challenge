f78d03c161bc6505aa371912879d7cee6d591ac4
Katy Pacheco <katypacheco@ravn.co>
2025-06-09 02:24:33 -0600

    feat: implement TaskFormModal for task creation and editing

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -2,10 +2,32 @@ import { RiAddLine } from "@remixicon/react";
 import { useState } from "react";
 import SwitchButton from "../switch-button/switch-button";
 import TaskColumns from "../task-columns/task-columns";
+import TaskFormModal from "../task-form-modal/task-form-modal";
 import styles from "./main-content.module.css";
+import { useCreateTaskMutation } from "../../graphql/mutations/create-task/create-task.graphql.generated";
+import type { CreateTaskInput, UpdateTaskInput } from "../../types/graphql";
 
 export default function MainContent() {
   const [viewMode, setViewMode] = useState<number | null>(1);
+  const [openModal, setOpenModal] = useState(false);
+  const [createTask] = useCreateTaskMutation();
+
+  const handleSubmit = async (
+    data: CreateTaskInput | Omit<UpdateTaskInput, "id">,
+  ) => {
+    try {
+      await createTask({
+        variables: {
+          input: {
+            ...data,
+          },
+        },
+      });
+      console.log("Task created!");
+    } catch (err) {
+      console.error("Error creating task", err);
+    }
+  };
 
   return (
     <div className={styles.mainContent}>
@@ -16,12 +38,20 @@ export default function MainContent() {
           type="button"
           aria-label="Add task"
           className={styles.topBarButtonAdd}
+          onClick={() => setOpenModal(true)}
         >
           <RiAddLine />
         </button>
       </div>
       {/* Tasks columns */}
       <TaskColumns viewMode={viewMode} />
+
+      <TaskFormModal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        mode="create"
+        onSubmit={handleSubmit}
+      />
     </div>
   );
 }
