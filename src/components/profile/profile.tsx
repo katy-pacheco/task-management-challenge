f0fafd12b6b950b080c405457df5a00871c396ff
Katy Pacheco <katypacheco@ravn.co>
2025-06-12 18:30:44 -0600

    feat: display profile creation and update dates in Profile component

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,4 +1,5 @@
 import { RiCloseLine } from "@remixicon/react";
+import { format } from "date-fns";
 import { useGetProfileQuery } from "../../graphql/queries/get-profile.graphql.generated";
 import Avatar from "../avatar/avatar";
 import styles from "./profile.module.css";
@@ -11,6 +12,10 @@ export default function Profile({ onClose }: ProfileProps) {
   const { data, loading, error } = useGetProfileQuery();
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :</p>;
+
+  const createdAt = data?.profile.createdAt;
+  const updatedAt = data?.profile.updatedAt;
+
   return (
     <div className={styles.profileContainer}>
       <button onClick={onClose}>
@@ -22,6 +27,14 @@ export default function Profile({ onClose }: ProfileProps) {
           <h2>{data.profile.fullName}</h2>
           <p>Email: {data.profile.email}</p>
           <p>Type: {data.profile.type}</p>
+          <p>
+            Created at:{" "}
+            {createdAt ? format(new Date(createdAt), "MMM.dd yyyy") : "N/A"}
+          </p>
+          <p>
+            Updated at:{" "}
+            {updatedAt ? format(new Date(updatedAt), "MMM.dd yyyy") : "N/A"}
+          </p>
         </div>
       )}
     </div>
